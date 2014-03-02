ae21d22fe385076b4563b644ad3be89452c0dcf1
Igor Vasilchikov <vasilchikov@gmail.com>
2014-03-01 21:14:09 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etcdemo::Application.config.secret_key_base = '53ccf35e9552f8427f693b9bfeb5e6616494c2aa5a5a3e88cec8f80c5f1a0536d4499a4a2069c4cf6edfb4dde0d8fda7d5588408a52492b1f55a6b0793b5a465'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Etcdemo::Application.config.session_store :cookie_store, key: '_etcdemo_session'
