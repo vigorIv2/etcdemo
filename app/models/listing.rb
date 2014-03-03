301c8a03d2e9702d75bc443f8ecc5f15b1c19a23
Igor Vasilchikov <vasilchikov@gmail.com>
2014-03-03 13:26:03 -0800

    added different storage for development vs. production

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,11 @@
 class Listing < ActiveRecord::Base
- has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "No_image_available.svg",
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+ if Rails.env.development?
+   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "No_image_available.svg"
+ else  
+   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "No_image_available.svg",
+      :storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+      :path => ":style/:id_:filename" 
+ end 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
