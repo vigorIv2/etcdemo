84ffa64da946b92bf196665e16abdb09277cb28c
Igor Vasilchikov <vasilchikov@gmail.com>
2014-03-10 21:09:26 -0700

    Add Seller page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Etcdemo::Application.routes.draw do
 
   get "pages/about"
   get "pages/contact"
+  get 'seller' => "listings#seller"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
